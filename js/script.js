2d8a52a09f4fe8bb0bb11b37a4dec67b42ed822f
darthNater <blstarwars@live.com>
2020-10-10 15:49:42 -0600

    added functionality so everything shoudl work and flow well

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var startBtn = document.getElementById('start-quiz');
 var highscoresBtn = document.getElementById('highscore-link');
 var homeBtn = document.getElementById('home-btn');
 var clearHighscoresBtn = document.getElementById('clear-scores');
+var submitHighscoreBtn = document.getElementById('submit-score')
 
 //My screens
 var homeScreen = document.querySelector('.start-screen');
@@ -14,18 +15,35 @@ var questionScreen = document.querySelector('.question-screen');
 var highscores = document.querySelector('.highscores');
 var questionTitle = document.querySelector('.question-title');
 var questionAnswers = document.querySelector('.question-answers');
+var timeLeftEl = document.querySelector('#time-left');
+var resultEl = document.querySelector('#result'); //Final quiz result element
+var feedbackEl = document.querySelector('#feedback'); //Feedback div
+var questionResultEl = document.querySelector('#questionResult'); //Result of chosen answer
 
 //My variables
-var quizLength = 10;
-var correctItems = 0;
+var quizLength = 10; //Number of questions
+var correctItems = 0; //How many they got right
 var currentQuiz = [];
 var currentQuestion = 0;
+var quizTimeLimit = 60; //In seconds
+var timeElapsed = 0;
+var timer;
+var feedbackTiemout; //Allows me to reset if they click answeres to quickly
+//Get the highscores from localStorage, if it's not there set this to a blank array
+var highscoresList = JSON.parse(localStorage.getItem("highscores")) || [];
+
+//Initials input
+var initialsInput = document.getElementById("initials");
 
 //Start the quiz
 function startQuiz() {
     //Get random questions up to the quiz list length (No duplicates)
     var tempArray = [...quiz];
     currentQuiz = [];
+    timeElapsed = 0;
+    currentQuestion = 0;
+    correctItems = 0;
+
     for(var i = 0; i < quizLength; i++)
     {
         //Get a random box within the tempArray length
@@ -41,28 +59,122 @@ function startQuiz() {
     //Put them on the first question
     showQuestion();
     getCurrentQuestion();
+
     //Start the timer
+    timer = setInterval(function () {
+        timeElapsed++;
+        displayTime()
+        if(timeElapsed >= quizTimeLimit)
+            endQuiz();
+	}, 1000);
 }
 
+//Clear the question screen and add the new question
 function getCurrentQuestion() {
     var quizItem = currentQuiz[currentQuestion];
     questionTitle.textContent = "Question " + (currentQuestion + 1) + ": " + quizItem.question;
     questionAnswers.innerHTML = '';
+    //Display each new answer
     quizItem.possibleAnswers.forEach((item, index) => {
         var questionAnswer = document.createElement("div");
-        questionAnswer.textContent = (index + 1) + ". " + item;
-        questionAnswer.setAttribute('value', index);
+        var questionAnswerContent = document.createElement("a");
+        questionAnswerContent.textContent = (index + 1) + ". " + item;
+        questionAnswerContent.setAttribute('value', index);
+        questionAnswerContent.classList.add('answer');
+        questionAnswer.append(questionAnswerContent);
         questionAnswers.append(questionAnswer);
     });
 }
 
+//Get the answer and go to the next question
+function answerQuestion(event) {
+    var chosenAnswer = parseInt(event.target.getAttribute('value'));
+    var questionResult = 'Wrong...';
+    if(chosenAnswer === currentQuiz[currentQuestion].correctAnswer)
+    {
+        correctItems++;
+        questionResult = "Correct!";
+        nextQuestion();
+    }
+    else
+    {
+        timeElapsed += 10;
+    }
+
+    questionResultEl.textContent = questionResult;
+
+    showFeedback();
+}
+
+//Go to the next question, if there are no more questions end the quiz
 function nextQuestion() {
     currentQuestion++;
+    if(currentQuestion == currentQuiz.length)
+    {
+        endQuiz();
+        return;
+    }
     getCurrentQuestion();
+}
+
+//End the quiz
+function endQuiz() {
+    //Stop the timer
+    window.clearInterval(timer);
+    //Calculate score
+    //If time left is < 0 set it to zero
+    var displayTimeLeft = quizTimeLimit - timeElapsed < 0 ? 0 : quizTimeLimit - timeElapsed;
+
+    resultEl.textContent = "You got " + correctItems + " right out of " + quizLength + " and finished with " + displayTimeLeft + " second(s) to spare";
+    showFinishScreen()
+}
+
+//Take the initials and add them to the highscore
+function submitHighscore() {
+    var initials = initialsInput.value || 'anonymous';
+    var highScore = {
+        initials: initials,
+        percentage: correctItems / quizLength, 
+        timeLeft: (quizTimeLimit - timeElapsed < 0 ? 0 : quizTimeLimit - timeElapsed),
+    }
+
+    highscoresList.push(highScore);
+    sortHighscores();
+    localStorage.setItem("highscores", JSON.stringify(highscoresList));
+    showHighscores();
+}
+
+//Sort the highscores by percentage, timeLeft, and initials
+function sortHighscores() {
+    highscoresList = highscoresList.sort((a, b) => {
+        if(a.percentage > b.percentage)
+            return -1;
+        if(a.percentage < b.percentage)
+            return 1;
+        if(a.timeLeft > b.timeLeft)
+            return -1;
+        if(a.timeLeft < b.timeLeft)
+            return 1;
+        if(a.initials < b.initials)
+            return -1;
+        if(a.initials > b.initials)
+            return 1;
+        return 0;
+    });
+}
+
+function populateHighscores() {
+    highscores.innerHTML = '';
+    highscoresList.forEach((item, i) => {
+        var highscore = document.createElement("div");
+        highscore.textContent = (i + 1) + ". " + item.initials + " " + (item.percentage * 100) + "% Time Left: " + item.timeLeft;  
+        highscores.append(highscore);
+    })
 }
 
 //Hide other screens and show highscores
 function showHighscores() {
+    populateHighscores();
     homeScreen.classList.add('d-none');
     finishScreen.classList.add('d-none');
     questionScreen.classList.add('d-none');
@@ -85,6 +197,7 @@ function showQuestion() {
     questionScreen.classList.remove('d-none');
 }
 
+//Hide other screens and show finish screen;
 function showFinishScreen() {
     homeScreen.classList.add('d-none');
     highscoreScreen.classList.add('d-none');
@@ -92,12 +205,29 @@ function showFinishScreen() {
     finishScreen.classList.remove('d-none');
 }
 
+//Show the feedback 
+function showFeedback() {
+    feedbackEl.classList.remove('d-none');
+    feedbackTimeout = setTimeout(() => {
+        feedbackEl.classList.add('d-none');
+    }, 2000);
+}
+
 //Clear Highscores
 function clearHighscores() {
     highscores.innerHTML = '';
+    highscoresList = [];
     localStorage.removeItem('highscores');
 }
 
+function displayTime() {
+    var timeLeft = quizTimeLimit - timeElapsed;
+    //if timeleft is less than zero set it to 0 instead of a negative number
+    timeLeft = timeLeft < 0 ? 0 : timeLeft;
+	timeLeftEl.textContent = timeLeft;
+}
+
+//Start us off
 showHome();
 
 //Awesome event listeners
@@ -105,6 +235,8 @@ startBtn.addEventListener("click", startQuiz);
 highscoresBtn.addEventListener("click", showHighscores);
 homeBtn.addEventListener("click", showHome);
 clearHighscoresBtn.addEventListener("click", clearHighscores);
+questionAnswers.addEventListener("click", answerQuestion);
+submitHighscoreBtn.addEventListener("click", submitHighscore);
 
 var quiz = [
     {
@@ -156,7 +288,7 @@ var quiz = [
     {
         question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
         possibleAnswers: ['if i <> 5', 'if i =! 5 then', 'if (i != 5)', 'if (i <> 5)'],
-        correctAnswer: 3,
+        correctAnswer: 2,
     },
     {
         question: 'How does a WHILE loop start?',
